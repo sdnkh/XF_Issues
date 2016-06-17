e7ff68196994dd47b483c9abe7c20d5328e36085
dinas <dinas@microsoft.com>
2016-06-17 14:58:53 -0700

    Searchbar issue.

---
+++ SearchbarIssue/SearchbarIssue/Pages/Page1.cs
@@ -0,0 +1,34 @@
+﻿using SearchbarIssue.ViewModels;
+using Xamarin.Forms;
+
+namespace SearchbarIssue
+{
+    public class Page1 : ContentPage
+    {
+        public Page1()
+        {           
+            SearchBar searchbar = new SearchBar
+            {
+                BackgroundColor = Color.Red
+            };
+
+            StackLayout layout = new StackLayout
+            {
+                Orientation = StackOrientation.Vertical,
+                Children = { searchbar }
+            };
+            layout.SetBinding(StackLayout.IsVisibleProperty, Page1ViewModel.IsLoadedPropertyName);
+
+            this.BindingContext = new Page1ViewModel();
+            this.Content = layout;
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+
+            Page1ViewModel vm = this.BindingContext as Page1ViewModel;
+            await vm.LoadData();
+        }
+    }
+}
---
+++ SearchbarIssue/SearchbarIssue/ViewModels/Page1ViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel;
+using System.Threading.Tasks;
+
+namespace SearchbarIssue.ViewModels
+{
+    public class Page1ViewModel : INotifyPropertyChanged
+    {
+        public const string IsLoadedPropertyName = "IsLoaded";
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private bool isLoaded;
+
+        public bool IsLoaded
+        {
+            get
+            {
+                return this.isLoaded;
+            }
+
+            set
+            {
+                this.isLoaded = value;
+                if (this.PropertyChanged == null)
+                {
+                    return;
+                }
+
+                this.PropertyChanged(this, new PropertyChangedEventArgs(IsLoadedPropertyName));
+            }
+        }
+
+        public async Task LoadData()
+        {
+            this.IsLoaded = false;
+            await Task.Delay(2000);
+            this.IsLoaded = true;
+        }
+    }
+}
